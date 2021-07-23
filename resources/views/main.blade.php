f8a7ad9fb3081dbf7f6d7f8a1891675a4cd28397
Pyotr <PrincesstaKamchapki@gmail.com>
2021-07-23 22:27:52 +0300

    Адаптация

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,36 +1,32 @@
 @extends('layout')
 
 @section('content')
-<div class='container'>
+<div class='container-sm'>
     <h1 class='mb-4'>Новости</h1>
     <div class='row'>
         @foreach ($news as $new)
-            <div class=' col-6 p-3 mb-3'>
+            <div class=' col-12 col-md-6 p-3 mb-3'>
                 <div class='card new shadow-sm'>
-                    <div class='row no-wrap'>
-                        <div class='new__head'>
-                            <div class='new__title-wrap'><h3 class='new__title text-truncate'>{{ $new->title }}</h3></div>
-                            <div class='new__title-comment-wrap text-muted'><strong class='new__title-comment text-truncate'>{{ $new->titleComment }}</strong></div>
-                            <div class='float-right new__categories text-muted text-truncate'>
-                            @foreach ($new->categories as $category)
-                                <div class='badge new__category' style='
-                                    background-color: {{ $category->background_color }};
-                                    color: {{ $category->font_color}};
-                                '>
-                                    {{ $category->name }}
-                                </div>
-                            @endforeach
+                    <div class='new__head no-wrap'>
+                        <div class='new__title-wrap'><h3 class='new__title text-truncate'>{{ $new->title }}</h3></div>
+                        <div class='new__title-comment-wrap text-muted'><strong class='new__title-comment text-truncate'>{{ $new->titleComment }}</strong></div>
+                        <div class='float-right new__categories text-muted text-truncate'>
+                        @foreach ($new->categories as $category)
+                            <div class='badge new__category' style='
+                                background-color: {{ $category->background_color }};
+                                color: {{ $category->font_color}};
+                            '>
+                                {{ $category->name }}
                             </div>
+                        @endforeach
                         </div>
                     </div>
                     <div class='new__content'>
-                        <div class='row'>
-                            <div class='col-6 new__image-wrap'>
-                                <img src='images/{{ $new->image }}' class='position-absolute left-0 img-fluid'>
-                            </div>
-                            <div class='col-6 w-100 new__text text-truncate'>
-                                {{ $new->description }}
-                            </div>
+                        <div class='new__image-wrap'>
+                            <img src='images/{{ $new->image }}' class='img-fluid'>
+                        </div>
+                        <div class='w-100 new__text text-truncate'>
+                            {{ $new->description }}
                         </div>
                     </div>
                 </div>
